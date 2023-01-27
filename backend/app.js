76a65a6c8adaac9b9e3bf0dc593bc6095742a1e9
Hamza-SE <muhammadhamza007123@gmail.com>
2023-01-28 02:53:40 +0500

    changed frontend url

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const errorMiddleware = require("./middlewares/error");
 const cors = require("cors");
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "https://full-stack-blog-app-frontend.netlify.app" }));
 app.use(morgan("dev"));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
